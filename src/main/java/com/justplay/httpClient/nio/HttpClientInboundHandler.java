26056f8e5f7dd804ed22ad09062c15e9aeb4d4ac
huangzezhou <764804602@qq.com>
2018-08-02 16:43:07 +0800

    nio模式HttpClient

---
+++ src/main/java/com/justplay/httpClient/nio/HttpClientInboundHandler.java
@@ -0,0 +1,48 @@
+package com.justplay.httpClient.nio;
+/**********************************************************************
+ * Copyright (c) 2018 CETC Company
+ * 中电科新型智慧城市研究院有限公司版权所有
+ * <p>
+ * PROPRIETARY RIGHTS of CETC Company are involved in the
+ * subject matter of this material. All manufacturing, reproduction, use,
+ * and sales rights pertaining to this subject matter are governed by the
+ * license agreement. The recipient of this software implicitly accepts
+ * the terms of the license.
+ * 本软件文档资料是中电科新型智慧城市研究院有限公司的资产，任何人士阅读和
+ * 使用本资料必须获得相应的书面授权，承担保密责任和接受相应的法律约束。
+ *************************************************************************/
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpResponse;
+
+/**
+ * @Package: com.justplay.httpClient
+ * @Project: justplayHttpClient
+ * @Description:   //TODO
+ * @Creator: huangzezhou
+ * @Create_Date: 2018/8/2 15:25
+ * @Updater: huangzezhou
+ * @Update_Date: 2018/8/2 15:25
+ * @Update_Description: huangzezhou 补充
+ **/
+public class HttpClientInboundHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (msg instanceof HttpResponse)
+        {
+            HttpResponse response = (HttpResponse) msg;
+            System.out.println("CONTENT_TYPE:" + response.headers().get(HttpHeaders.Names.CONTENT_TYPE));
+        }
+        if(msg instanceof HttpContent)
+        {
+            HttpContent content = (HttpContent)msg;
+            ByteBuf buf = content.content();
+            System.out.println(buf.toString(io.netty.util.CharsetUtil.UTF_8));
+            buf.release();
+        }
+    }
+}
